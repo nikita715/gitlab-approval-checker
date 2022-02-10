2deed11ee4f7869ba36c4aa898b28f0d8677f97b
Nikita Stepochkin <n.stepochkin@headsandhands.ru>
2022-02-10 16:26:53 +0300

    feat: logging

--- approval-check.sh
+++ approval-check.sh
@@ -1,8 +1,10 @@
 MR_DATA=$(curl --silent --request GET --header \"PRIVATE-TOKEN: $PROJECT_PRIVATE_TOKEN\" ${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/merge_requests/${CI_MERGE_REQUEST_IID})
+echo "MR_DATA=$MR_DATA"
 
 read -a arr < <(echo $(echo $MR_DATA | jq -r '.upvotes'))
 
 MR_UPVOTES=${arr[0]}
+echo "MR_UPVOTES=$MR_UPVOTES"
 
 if [ "${MR_UPVOTES}" -ge "$(expr ${PROJECT_UPVOTES_QUANTITY})" ];
   then
