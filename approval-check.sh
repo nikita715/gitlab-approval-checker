8893c642817ac2e18b0dc1f518f3c640c60312f6
Nikita Stepochkin <n.stepochkin@headsandhands.ru>
2022-02-10 12:58:06 +0300

    feat: core functionality

---
+++ approval-check.sh
@@ -0,0 +1,13 @@
+MR_DATA=$(curl --silent --request GET --header \"PRIVATE-TOKEN: $PROJECT_PRIVATE_TOKEN\" ${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/merge_requests/${CI_MERGE_REQUEST_IID})
+
+read -a arr < <(echo $(echo $MR_DATA | jq -r '.upvotes'))
+
+MR_UPVOTES=${arr[0]}
+
+if [ "${MR_UPVOTES}" -ge "$(expr ${PROJECT_UPVOTES_QUANTITY})" ];
+  then
+    echo "MR OK";
+  else
+    echo "MR ERROR Need more votes";
+    exit 1;
+fi
