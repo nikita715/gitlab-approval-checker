f60b53a47b536cc8ef27fed8e0df7b406a76b528
Nikita Stepochkin <n.stepochkin@headsandhands.ru>
2022-02-10 18:32:35 +0300

    feat: optional logging

--- script/approval-check.sh
+++ script/approval-check.sh
@@ -1,10 +1,12 @@
+APPROVAL_CHECKER_DEBUG=${APPROVAL_CHECKER_DEBUG:-false}
+
 MR_DATA=$(curl --silent --request GET --header "PRIVATE-TOKEN: $PROJECT_PRIVATE_TOKEN" "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/merge_requests/${CI_MERGE_REQUEST_IID}")
-echo "MR_DATA=$MR_DATA"
+if [[ "$APPROVAL_CHECKER_DEBUG" == "true" ]]; then echo "MR_DATA=$MR_DATA" ; fi;
 
 read -a arr < <(echo $(echo $MR_DATA | jq -r '.upvotes'))
 
 MR_UPVOTES=${arr[0]}
-echo "MR_UPVOTES=$MR_UPVOTES"
+if [[ "$APPROVAL_CHECKER_DEBUG" == "true" ]]; then echo "MR_UPVOTES=$MR_UPVOTES" ; fi;
 
 if [ "${MR_UPVOTES}" -ge "$(expr ${PROJECT_UPVOTES_QUANTITY})" ];
   then
