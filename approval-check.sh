8b695324562c974b1172ef017701a1ac89e8b1ac
Nikita Stepochkin <n.stepochkin@headsandhands.ru>
2022-02-10 16:30:57 +0300

    fix: mr request

--- approval-check.sh
+++ approval-check.sh
@@ -1,4 +1,4 @@
-MR_DATA=$(curl --silent --request GET --header \"PRIVATE-TOKEN: $PROJECT_PRIVATE_TOKEN\" ${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/merge_requests/${CI_MERGE_REQUEST_IID})
+MR_DATA=$(curl --silent --request GET --header "PRIVATE-TOKEN: $PROJECT_PRIVATE_TOKEN" "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/merge_requests/${CI_MERGE_REQUEST_IID}")
 echo "MR_DATA=$MR_DATA"
 
 read -a arr < <(echo $(echo $MR_DATA | jq -r '.upvotes'))
